ce1cb0f443753a5268f283ab5522f757c2fe0f95
LucaStellato <lucastellato435@gmail.com>
2025-12-10 12:26:15 +0100

    defined routes

---
+++ routers/films.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router();
+const filmController = require('../controllers/filmController')
+
+//index
+router.get('/', filmController.index);
+
+//show
+router.get('/:id', filmController.show);
