44014f8cbe5087590b9ba28b4708065ed496a474
LucaStellato <lucastellato435@gmail.com>
2025-12-11 14:23:09 +0100

    import cors

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require("cors")
 const filmsRouter = require('./routers/films')
 const notFound = require('./middleware/errorHandler')
 
 
 app.use(express.json())
+
+app.use(cors({
+    origin: 'http://localhost:5173'
+}))
+
 
 app.get('/', (req, res) => {
     res.send('SERVER IS ACTIVE')
