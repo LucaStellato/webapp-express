5c5d8008dbf1ae44302e7c4876792a49d70f54bb
LucaStellato <lucastellato435@gmail.com>
2025-12-15 11:54:04 +0100

    create function storeReview

--- controllers/filmController.js
+++ controllers/filmController.js
@@ -9,14 +9,24 @@ function index(req, res) {
 }
 
 function show(req, res) {
-    const id = req.params.id
-    const sql = 'SELECT * FROM movies WHERE id=?'
+    const id = req.params.id;
+    const sql = 'SELECT * FROM movies WHERE id=?';
+    const reviewSql = 'SELECT * FROM reviews WHERE movie_id=?';
     connection.query(sql, [id], (err, results) => {
         if (err) return res.status(500).json({ error: 'failed' });
-        if (results.length === 0) return res.status(404).json({ error: 'movie not found' })
-        res.json(results[0])
-    })
+        if (results.length === 0) return res.status(404).json({ error: 'movie not found' });
+        const movie = results[0];
+        connection.query(reviewSql, [id], (err, reviewResults) => {
+            if (err) return res.status(500).json({ error: 'failed' });
+            movie.reviews = reviewResults;
+            res.json(movie);
+        });
+    });
 }
 
+const storeReview = (res, req) => {
+    const movieId = Number(req.params.id)
+    const { name, vote, text } = req.body
+}
 
-module.exports = { index, show }
+module.exports = { index, show };
